e4a5cba5516d6ff68b8d7f995025e42cb65b8393
C. Clauson <coclauso@microsoft.com>
2023-10-07 02:50:21 -0700

    Initial file commit

---
+++ TechnicalAnalysis.cs
@@ -0,0 +1,79 @@
+using Azure;
+using Azure.Data.Tables;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ThreeFourteen.Finnhub.Client;
+using ThreeFourteen.Finnhub.Client.Model;
+
+namespace cclauson
+{
+    public class BaseTableEntity : ITableEntity {
+        public string? PartitionKey { get; set; }
+        public string? RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+
+    public class ValueEntity : BaseTableEntity {
+        public decimal? value { get; set; }
+        public DateTime? dateTime { get; set; }
+    }
+
+    public class TechnicalAnalysis
+    {
+        private readonly ILogger _logger;
+        private readonly FinnhubClient finnhubClient;
+        private static readonly string connectionString = "%StorageConnectionString%";
+        private readonly TableClient tableClient = new TableClient(connectionString, "values");
+
+        public TechnicalAnalysis(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TechnicalAnalysis>();
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+            // Based on:
+            // https://swharden.com/blog/2021-10-09-console-secrets/
+            // see for instructions on how to add client key value
+            var finnhubClientKey = config["finnhub-client-key"];
+            if (finnhubClientKey == null) {
+                throw new KeyNotFoundException("Couldn't find finnhub client key");
+            }
+            finnhubClient = new(finnhubClientKey);
+        }
+
+        [Function("TechnicalAnalysis")]
+        public Task Run([TimerTrigger("%TimerSchedule%")] MyInfo myTimer)
+        {
+            return this.RunI(myTimer);
+        }
+
+        private async Task RunI(MyInfo myTimer) {
+            Quote quote = await this.finnhubClient.Stock.GetQuote("MSFT");
+            decimal price = quote.Current;
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            _logger.LogInformation($"Current quote: {price}");
+            this.tableClient.AddEntity<ValueEntity>(new ValueEntity() {
+                PartitionKey = "TestPartitionKey",
+                RowKey = DateTime.UtcNow.ToString("yyyyMMddHHmmssfffffff"),
+                value = price,
+                dateTime = DateTime.UtcNow
+            });
+        }
+    }
+
+    public class MyInfo
+    {
+        public MyScheduleStatus ScheduleStatus { get; set; }
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
